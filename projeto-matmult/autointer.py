2b1756262ad7ded870a08a5672cb435bf3a8b226
Danilo Ramos <danilocr@al.insper.edu.br>
2025-03-22 17:26:31 -0300

    Autointer com os valores de k e funcao de b

--- projeto-matmult/autointer.py
+++ projeto-matmult/autointer.py
@@ -11,18 +11,34 @@ def x(t):
 def y(t):
     return 7*m.sin(t)-3*m.sin(6*t)
 
+def f(b, k):
+    return (-14 * m.sin(((-2*m.pi/5) * k) / 2) * m.sin(b)) - (6 * m.cos(((-2*m.pi/5) * k) * 3) * m.sin(3*b))
+
+valores_k = [-1, -2, -3, -4, -5]
+
+
 pontos = []
 inter = []
 for t in intervalo_t:
     pontos.append([x(t),y(t)])
 
+"""
 for i in range(len(pontos)):
     for j in range(len(pontos)):
         if i != j:
             difx = abs(pontos[i][0]-pontos[j][0])
             dify = abs(pontos[i][1]-pontos[j][1])
+        
             if (difx <= tol) and (dify <= tol):
                 inter.append(intervalo_t[i])
+"""
+
+for b in intervalo_t:
+    for k in valores_k:
+        func = f(b,k)
+        if (func == 0) or (func <= 0 + tol and func >= 0 - tol):
+            inter.append(b)
+
 #print(pontos)
 
 print(inter)
