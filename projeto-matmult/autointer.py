f85805ace8d974c3f174848c20523e8ab19876e0
denisalonso <denisalonso1999@gmail.com>
2025-03-22 14:59:24 -0300

    autointersecao

--- projeto-matmult/autointer.py
+++ projeto-matmult/autointer.py
@@ -3,30 +3,27 @@ import numpy as np
 
 intervalo_t = np.arange(0,2*m.pi,0.01)
 
-verif = 0.00000000000000001
+tol = 0.08
 
 def x(t):
-    return m.cos(t)-3*m.cos(6*t)
+    return 7*m.cos(t)-3*m.cos(6*t)
 
 def y(t):
-    return m.sin(t)-3*m.sin(6*t)
+    return 7*m.sin(t)-3*m.sin(6*t)
 
 pontos = []
 inter = []
 for t in intervalo_t:
-    if len(pontos) != 0:
-        ponto_novo = [x(t),y(t)]
-        pontos.append(ponto_novo)
-        for i in range(len(pontos)):
-            difx = abs(pontos[i][0]-ponto_novo[0])
-            dify = abs(pontos[i][1]-ponto_novo[1])
-            #print([difx,dify])
-            if (difx <= 0.01) and (dify <= 0.01):
-                #print([difx,dify])
-                inter.append(t)
-    else:
-        pontos.append([x(t),y(t)])
+    pontos.append([x(t),y(t)])
 
-print(pontos)
+for i in range(len(pontos)):
+    ponto_comparacao = pontos[i]
+    for j in range(len(pontos)):
+        if i != j:
+            difx = abs(pontos[i][0]-pontos[j][0])
+            dify = abs(pontos[i][1]-pontos[j][1])
+            if (difx <= tol) and (dify <= tol):
+                inter.append(intervalo_t[j])
+#print(pontos)
 
-# print(inter)
+print(inter)
