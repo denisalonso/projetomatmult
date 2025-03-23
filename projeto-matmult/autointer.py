a746226d3aaaf1ac2e92a4adc56bd953dae5cbb3
denisalonso <denisalonso1999@gmail.com>
2025-03-23 15:06:04 -0300

    leves ajustes

--- projeto-matmult/autointer.py
+++ projeto-matmult/autointer.py
@@ -1,9 +1,10 @@
 import math as m
 import numpy as np
 
-intervalo_t = np.arange(0,2*m.pi,0.01)
+intervalo_u = np.arange(0,2*m.pi,0.1)
+intervalo_v = np.arange(0,2*m.pi,0.1)
 
-tol = 0.05
+tol = 0.001
 
 def x(t):
     return 7*m.cos(t)-3*m.cos(6*t)
@@ -11,18 +12,15 @@ def x(t):
 def y(t):
     return 7*m.sin(t)-3*m.sin(6*t)
 
-pontos = []
 inter = []
-for t in intervalo_t:
-    pontos.append([x(t),y(t)])
+for u in intervalo_u:
+    for v in intervalo_v:
+        errox = abs(x(u)-x(v))
+        erroy = abs(x(u)-x(v))
+        if errox <= tol and erroy <= tol and u!=v:
+            print(errox,erroy, [u,v])
+            inter.append(u)
+            # inter.append(v)
 
-for i in range(len(pontos)):
-    for j in range(len(pontos)):
-        if i != j:
-            difx = abs(pontos[i][0]-pontos[j][0])
-            dify = abs(pontos[i][1]-pontos[j][1])
-            if (difx <= tol) and (dify <= tol):
-                inter.append(intervalo_t[i])
-#print(pontos)
-
-print(inter)
+# for int in inter:
+#     print(int)
