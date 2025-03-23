833f411cc511cb02236515f6e9f81afa94889570
denisalonso <denisalonso1999@gmail.com>
2025-03-23 15:18:17 -0300

    tangentes funcional

--- projeto-matmult/tangentes2.py
+++ projeto-matmult/tangentes2.py
@@ -5,7 +5,8 @@ intervalo_t = np.arange(0,2*m.pi,0.0001)
 
 ver = []
 hor = []
-verif = 0.008
+verif = 0.005
+
 for t in intervalo_t:
     xlinha = -7*m.sin(t) + 18*m.sin(6*t)
     ylinha = 7*m.cos(t) - 18*m.cos(6*t)
@@ -13,11 +14,19 @@ for t in intervalo_t:
     errox = abs(xlinha)
     erroy = abs(ylinha)
 
-    if errox <= verif:
+    if errox <= verif and erroy > verif:
         ver.append(t)
     
-    if erroy <= verif:
+    if erroy <= verif and errox > verif:
         hor.append(t)
 
-print(ver)
-print(hor)
+# print(ver)
+# print(hor)
+
+for u in ver:
+    print(u, end=' / ')
+print()
+print('---------------------------------------------------')
+
+for v in hor:
+    print(v, end=' / ')
